13db275911d1325254123ca21d49571cfd91c864
hitehiters-lang <hitehiters@gmail.com>
2026-03-24 15:40:52 +0300

    added logs and config

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"golang.design/x/clipboard"
+)
+
+type Configuration struct {
+	SaveDir string `json:"saveDir"`
+}
+
+func main() {
+	execPath, _ := os.Executable()
+	execDir := filepath.Dir(execPath)
+
+	logFile := filepath.Join("goshot.log")
+	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("open log error: ", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	log.SetFlags(log.Ldate | log.Ltime)
+
+	log.Println("goshot launched at", execDir)
+
+	configPath := filepath.Join(execDir, "config.json")
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		configPath = "config.json"
+		log.Println("got config")
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		log.Fatalln("config open error:", err)
+		return
+	}
+	defer file.Close()
+	log.Println("opened config")
+
+	conf := Configuration{}
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&conf)
+	if err != nil {
+		log.Fatalln("confing parsing error:", err)
+		return
+	}
+	log.Println("saveDir path readed from config:", conf.SaveDir)
+
+	if err = os.MkdirAll(conf.SaveDir, os.ModePerm); err != nil {
+		log.Fatalln("mkdir error:", err)
+		return
+	}
+
+	var lastSize int
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	if err := clipboard.Init(); err != nil {
+		log.Fatalln("clipboard init error:", err)
+		return
+	}
+	log.Println("clipboard inited")
+
+	for {
+		<-ticker.C
+
+		data := clipboard.Read(clipboard.FmtImage)
+		if data == nil {
+			continue
+		}
+
+		if len(data) == lastSize {
+			continue
+		}
+		lastSize = len(data)
+
+		filename := filepath.Join(conf.SaveDir, fmt.Sprintf("goshot_%s.png", time.Now().Format("20060102_150405")))
+
+		if err := os.WriteFile(filename, data, 0644); err != nil {
+			log.Fatalln("write file error:", err)
+			continue
+		}
+
+		log.Printf("saved: %s (%.2f KB)\n", filepath.Base(filename), float64(len(data))/1024)
+	}
+}
+
+// building with
+// go build -ldflags="-H windowsgui" -o GoShot.exe
